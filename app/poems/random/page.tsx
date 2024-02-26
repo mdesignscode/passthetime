92fdf2da71ea7401ffcccfbd519490db3583eb2e
Mdesignscode <marlonbaatjes@gmail.com>
2024-02-27 01:46:44 +0200

    🪄

--- app/poems/random/page.tsx
+++
@@ -1,9 +0,0 @@
-import prisma from "lib/prisma";
-import RandomPoem from "./Components/RandomPoem";
-
-export default async function Page() {
-  const poems = await prisma.poem.findMany({ include: { Author: true } }),
-    randomIndex = Math.floor(Math.random() * poems.length);
-
-  return <RandomPoem poems={poems} initialIndex={randomIndex} />;
-}
